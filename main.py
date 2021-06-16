16b62868f7e9d97bbc43cea97b869f43bd236cbc
rahulx81x <rahulgouri072@gmailcom>
2021-06-16 20:56:01 +0530

    added support for next round of game and delay time before terminating

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import turn
+import time
 from dices import d
 
 
@@ -9,25 +10,32 @@ class Player:
         self.position = position
 
 
-count = int(input('Enter no. of players(<=4 for better experience): '))
-P = []   # Player List
+consent = 'y'
+while consent == 'y':
+    count = int(input('\nEnter no. of players(<=4 for better experience): '))
+    P = []  # Player List
 
-
-for index in range(count):
-    P.append(Player(index, 0))
-winner = 0
-while winner == 0:
     for index in range(count):
-        while P[index].position != 100:
-            print("Your turn : Player", end=" ")
-            print(P[index].number + 1)
-            dice = int(input('Enter Dice Number(1, 2, 3, 4): '))
-            throw = int(random.choice(d[dice - 1]))
-            turn.turn(P[index], throw)
-            if P[index].position == 100:
-                winner = 1
-            break
-print("GAME HAS ENDED. CONGRATULATIONS TO THE WINNER. HOPE YOU ALL ENJOYED!!")
+        P.append(Player(index, 0))
+    winner = 0
+    while winner == 0:
+        for index in range(count):
+            while P[index].position != 100:
+                print("Your turn : Player", end=" ")
+                print(P[index].number + 1)
+                dice = int(input('Enter Dice Number(1, 2, 3, 4): '))
+                throw = int(random.choice(d[dice - 1]))
+                turn.turn(P[index], throw)
+                if P[index].position == 100:
+                    winner = 1
+                break
+    print("GAME HAS ENDED. CONGRATULATIONS TO THE WINNER. HOPE YOU ALL ENJOYED!!")
+    consent = input('\n HEY! Let\'s play again \nInput y for Yes, anything else otherwise: ')
+
+
+print('\n Hope to see you soon')
 print("-rahulx81x")
+
+time.sleep(3)
 # rahulx81x
 
